2a7bc3b84ff1c3375a1aa2f26014af2c6059fe77
mustapha-drame <mustapha.drame@city.ac.uk>
2024-10-21 19:43:24 +0100

    Update task1.sql

--- HW 4/task1.sql
+++ HW 4/task1.sql
@@ -4,8 +4,8 @@ USE pet_database;
 
 DROP TABLE IF EXISTS petPet, petEvent;
 
-CREATE TABLE petPet (PRIMARY KEY petName VARCHAR(20), owner VARCHAR(45), species CHAR(1) CHECK (species IN ('M', 'F')), 
-gender VARCHAR(1), birth DATE() NOT NULL, death DATE());
+CREATE TABLE petPet (PRIMARY KEY petName VARCHAR(20), owner VARCHAR(45), species VARCHAR(15), 
+gender CHAR(1) CHECK (species IN ('M', 'F')), birth DATE() NOT NULL, death DATE());
 
 CREATE TABLE petEvent (petName) VARCHAR(20), eventDate DATE(), eventType VARCHAR(15), remark VARCHAR(255),
 PRIMARY KEY (petName, eventDate), FOREIGN KEY (petName) REFERENCES petPet (petName));
