9638f3c3007ff5f4823541baa6bfff2d5d6ce7c9
mustapha-drame <mustapha.drame@city.ac.uk>
2024-10-21 19:43:28 +0100

    Update task2.sql

--- HW 4/task2.sql
+++ HW 4/task2.sql
@@ -5,12 +5,12 @@ INSERT INTO petEvent (petName, eventDate, eventType, remark) VALUES
     ('Claws', '1997-08-03', 'injury', 'broke the rib');
 
 INSERT INTO petPet (petName, owner, species, gender, birth, death)
-    ('Hammy', 'Diane', 'M', 'hamster', '2010-10-30', NULL),
-    ('Kitten1', 'Fluffy', 'M', NULL, CURRENT_DATE(), NULL),
-    ('Kitten2', 'Fluffy', 'M', NULL, CURRENT_DATE(), NULL),
-    ('Kitten3', 'Fluffy', 'F', NULL, CURRENT_DATE(), NULL),
-    ('Kitten4', 'Fluffy', 'F', NULL, CURRENT_DATE(), NULL),
-    ('Kitten5', 'Fluffy', 'F', NULL, CURRENT_DATE(), NULL);
+    ('Hammy', 'Diane', 'hamster', 'M', '2010-10-30', NULL),
+    ('Kitten1', 'Fluffy', NULL, 'M', CURRENT_DATE(), NULL),
+    ('Kitten2', 'Fluffy', NULL, 'M', CURRENT_DATE(), NULL),
+    ('Kitten3', 'Fluffy', NULL, 'F', CURRENT_DATE(), NULL),
+    ('Kitten4', 'Fluffy', NULL, 'F', CURRENT_DATE(), NULL),
+    ('Kitten5', 'Fluffy', NULL, 'F', CURRENT_DATE(), NULL);
 
 UPDATE petPet
 SET death = '2020-09-01'
